a0c5925fac7b11f10732e158203cb484c71467e2
Manuel Antunes <manuelalexandre.balboaantunes@dcmail.ca>
2025-02-17 00:50:28 -0500

    Add files via upload

---
+++ Assignment.java
@@ -0,0 +1,11 @@
+/*   Name: Manuel Antunes
+     Date: 2025-02-17
+     Description: This program calculates the user's input temperatures for each day they choose.
+                  It checks the input for correctness and calculates the daily averages,
+                  as well as the highest and lowest temperatures during the specified days.
+*/
+
+import java.util.Scanner;
+public class Assignment {
+
+}
