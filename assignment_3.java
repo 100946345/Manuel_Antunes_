aae47f007cb436ab2ce964392f70223333c69f4e
100946345 <manuelalexandre.balboaantunes@dcmail.ca>
2025-03-09 21:28:57 -0400

    Update assignment_3.java

--- assignment_3.java
+++ assignment_3.java
@@ -6,8 +6,13 @@
 import java.util.Scanner;
 
 public class assignment_3 {
+    static Scanner input = new Scanner(System.in);
+
+    static final int FRAMES = 10;
+    static final int PINS = 10;
+
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+
         System.out.println("[Insert the name of each player]");
         System.out.print("Player (1) - ");
         String player_1 = input.nextLine();
@@ -17,20 +22,32 @@ public class assignment_3 {
         String player_3 = input.nextLine();
 
 
+        int[] player1_score = getGameScores(player_1);
+        int[] player2_score = getGameScores(player_2);
+        int[] player3_score = getGameScores(player_3);
+
+
+
 
 
         input.close();
     }
 
-    static void getRoll(){
-
+    static int getRoll(){
+        int roll = input.nextInt();
+        while (roll < 0 || roll > 10){
+            System.out.println("Try again by entering a score between 0 and 10 - ");
+            roll = input.nextInt();
+        }
+        input.nextLine();
+        return roll;
     }
 
     static void getTurnScores(){
 
     }
 
-    static void getGameScores(){
+    static void getGameScores(String player){
 
     }
 
