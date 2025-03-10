5305df95245795f0cf5cb8d1876aae488a210e76
100946345 <manuelalexandre.balboaantunes@dcmail.ca>
2025-03-09 20:47:26 -0400

    Update assignment_3.java

--- assignment_3.java
+++ assignment_3.java
@@ -1,2 +1,37 @@
+/*   Name: Manuel Antunes
+     Date: 2025-03-09
+     Description:
+*/
+
+import java.util.Scanner;
+
 public class assignment_3 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("[Insert the name of each player]");
+        System.out.print("Player (1) - ");
+        String player_1 = input.nextLine();
+        System.out.print("Player (2) - ");
+        String player_2 = input.nextLine();
+        System.out.print("Player (3) - ");
+        String player_3 = input.nextLine();
+
+
+
+
+        input.close();
+    }
+
+    static void getRoll(){
+
+    }
+
+    static void getTurnScores(){
+
+    }
+
+    static void getGameScores(){
+
+    }
+
 }
