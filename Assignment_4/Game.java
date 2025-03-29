288f7918f6de056b82e66bc71c0560e0db297f46
100946345 <manuelalexandre.balboaantunes@dcmail.ca>
2025-03-29 14:33:37 -0400

    Update Game.java

--- Assignment_4/Game.java
+++ Assignment_4/Game.java
@@ -3,6 +3,25 @@ public class Game {
     private int frame = 1;
     private Player winner;
 
+    public void playTurn(Player player, int score) {
+
+        player.setScore(player.getScore() + score);
+        frame++;
+    }
+
+    public int getFrame() {
+        return frame;
+    }
+
+    public boolean isComplete() {
+        return frame > 10;
+    }
+
+    public Player getWinner() {
+        return winner;
+    }
+
+
     public String toString() {
 
         return "";
