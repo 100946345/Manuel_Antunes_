2d3065d8b193af9ac9e8ac9a05dd1c2561c5e895
100946345 <manuelalexandre.balboaantunes@dcmail.ca>
2025-03-29 03:50:02 -0400

    Update Player.java

--- Assignment_4/Player.java
+++ Assignment_4/Player.java
@@ -6,6 +6,40 @@ class Player {
 
     public String toString() {
 
+        public Player(String name) {
+            if (name.trim().isEmpty() || name.matches(".*[!@#$%^&*/|?\\\\<>=\'\"].*")) {
+                throw new IllegalArgumentException("The name can't be empty or contain inappropriate words.");
+            }
+            this.name = name;
+
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getWins() {
+            return wins;
+        }
+
+        public int getScore() {
+            return score;
+        }
+
+
+        public  void setWins(int wins){
+            if (wins<0){
+                throw new IllegalArgumentException ("Wins can't be negative");
+            }this.wins = wins;
+        }
+
+        public  void setScore(int score){
+            if (score<0){
+                throw new IllegalArgumentException ("The score can't be negative");
+            }this.score = score;
+        }
+
+
         return "                \n[Player]\n" +
                 "                Name - " + name + "\n" +
                 "                Wins - " + wins + "\n" +
