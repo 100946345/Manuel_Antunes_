30f037278e465bae11f9a67985eef41da8d473d2
100946345 <manuelalexandre.balboaantunes@dcmail.ca>
2025-03-29 14:32:32 -0400

    Update Player.java

--- Assignment_4/Player.java
+++ Assignment_4/Player.java
@@ -4,46 +4,48 @@ public class Player {
     private int wins = 0;
     private int score = 0;
 
-    public String toString() {
 
-        public Player(String name) {
-            if (name.trim().isEmpty() || name.matches(".*[!@#$%^&*/|?\\\\<>=\'\"].*")) {
-                throw new IllegalArgumentException("The name can't be empty or contain inappropriate words.");
-            }
-            this.name = name;
-
+    public Player(String name) throws CustomValidationException {
+        if (name.trim().isEmpty() || name.matches(".*[!@#$%^&*/|?\\\\<>=\'\"].*")) {
+            throw new IllegalArgumentException("The name can't be empty or contain inappropriate words.");
         }
+        this.name = name;
 
-        public String getName() {
-            return name;
-        }
+    }
 
-        public int getWins() {
-            return wins;
-        }
+    public String getName() {
+        return name;
+    }
 
-        public int getScore() {
-            return score;
-        }
+    public int getWins() {
+        return wins;
+    }
 
-
-        public  void setWins(int wins){
-            if (wins<0){
-                throw new IllegalArgumentException ("Wins can't be negative");
-            }this.wins = wins;
-        }
-
-        public  void setScore(int score){
-            if (score<0){
-                throw new IllegalArgumentException ("The score can't be negative");
-            }this.score = score;
-        }
+    public int getScore() {
+        return score;
+    }
 
 
+    public  void setWins(int wins){
+        if (wins<0){
+            throw new IllegalArgumentException ("Wins can't be negative");
+        }this.wins = wins;
+    }
+
+    public  void setScore(int score){
+        if (score<0){
+            throw new IllegalArgumentException ("The score can't be negative");
+        }this.score = score;
+    }
+
+    public String toString(){
         return "                \n[Player]\n" +
                 "                Name - " + name + "\n" +
                 "                Wins - " + wins + "\n" +
                 "                Score - " + score+"\n";
 
     }
+
 }
+
+
