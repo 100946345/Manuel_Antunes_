958e34d35ab9edd3bd6caf36bd94167bb482d4b3
100946345 <manuelalexandre.balboaantunes@dcmail.ca>
2025-03-29 14:37:25 -0400

    .

--- Assignment_4/Throw.java
+++ Assignment_4/Throw.java
@@ -3,4 +3,22 @@ public class Throw {
     private int pins;
     private boolean isStrike;
     private boolean isSpare;
+
+    public Throw(int pins) {
+        this.pins = pins;
+        this.isStrike = (pins == 10);
+    }
+
+    public int getPins() {
+        return pins;
+    }
+
+    public boolean isStrike() {
+        return isStrike;
+    }
+
+    public boolean isSpare() {
+        return isSpare;
+
+    }
 }
--- Assignment_4/Turn.java
+++ Assignment_4/Turn.java
@@ -4,6 +4,13 @@ public class Turn {
     private Throw throw_two;
     private int score;
 
+    public Turn(Throw throwOne, Throw throwTwo) {
+        this.throwOne = throwOne;
+        this.throwTwo = throwTwo;
+        this.score = throwOne.getPins() + throwTwo.getPins();
+    }
+
+
     public String toString() {
 
         return "";
