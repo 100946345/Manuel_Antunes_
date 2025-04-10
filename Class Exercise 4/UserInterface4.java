3f596456fc338e67118df681e9916ac85e09f65d
100946345 <manuelalexandre.balboaantunes@dcmail.ca>
2025-04-10 11:54:30 -0400

    Update UserInterface4.java

--- Class Exercise 4/UserInterface4.java
+++ Class Exercise 4/UserInterface4.java
@@ -10,7 +10,9 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
 import java.io.FileWriter;
+import java.io.FileReader;
 import java.io.IOException;
 
 public class UserInterface4 {
