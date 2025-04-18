919b83d2cdb115debd1032db2aa1a8ca0dd28e1e
100946345 <manuelalexandre.balboaantunes@dcmail.ca>
2025-04-18 11:10:30 -0400

    Update UserInterface_Final.java

--- Project_UserInterface/UserInterface_Final.java
+++ Project_UserInterface/UserInterface_Final.java
@@ -23,9 +23,47 @@ import java.util.regex.Pattern;
 
 
 public class UserInterface_Final {
+    //window frame
+    private final JFrame frame;
+    //all the components (textfield)
+    private final JTextField txtPatient;
+    private final JTextField txtOwner;
+    private final JTextField txtEmail;
+    //set the buttons
+    private final JRadioButton rbAntunes;
+    private final JRadioButton rbBalboa;
+    //errors message
+    private final JLabel lblMessage;
 
 
-    public static void main(String[] args) {
+    public UserInterfaceFinal() {
+        frame = new JFrame("Veterinary clinic [Patient registration]");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(600, 500);
+        //variable that let me edit the layout
+        frame.setLayout(new GridBagLayout());
+        //creates the object for the layout position
+        GridBagConstraints layout = new GridBagConstraints();
+        //space 10px
+        layout.insets = new Insets(10, 10, 10, 10);
+        layout.fill = GridBagConstraints.HORIZONTAL;
+
+        // Font Sizes
+        Font labelFont = new Font("Arial", Font.BOLD, 20);
+        Font inputFont = new Font("Arial", Font.PLAIN, 18);
+        Dimension inputSize = new Dimension(350, 40);
+
+
+
+
+
+
+
+
+
+
+
+        public static void main(String[] args) {
         // runs Swing Event Dispatch Thread
         SwingUtilities.invokeLater(UserInterfaceFinal::new);
     }
